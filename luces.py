4bebf6868d5fda53a3afdd62da344b24d2125bcc
m1wrt <tottytortinha@gmail.com>
2025-03-18 16:33:44 -0400

    Create luces.py

---
+++ luces.py
@@ -0,0 +1,47 @@
+# Imports go at the top
+from microbit import *
+
+
+# Code in a 'while True:' loop repeats forever
+while True:
+    pin0.write_digital()
+    if pin14.read_digital() == 1:
+            display.show(Image.ARROW_N)
+            pin0.write_digital(1)
+            sleep(1000)
+            pin0.write_digital(0)
+            sleep(1000)
+            pin1.write_digital(1)
+            sleep(1000)
+            pin1.write_digital(0)
+            sleep(1000)
+            pin2.write_digital(1)
+            sleep(1000)
+            pin2.write_digital(0)
+            sleep(1000)
+
+
+            
+    if pin13.read_digital() == 1:
+            display.show(Image.ARROW_S)
+            pin2.write_digital(1)
+            sleep(1000)
+            pin2.write_digital(0)
+            sleep(1000)
+            pin1.write_digital(1)
+            sleep(1000)
+            pin1.write_digital(0)
+            sleep(1000)
+            pin0.write_digital(1)
+            sleep(1000)
+            pin0.write_digital(0)
+            sleep(1000)
+
+
+    else:
+            display.show(Image.ANGRY)
+            pin0.write_digital(0)
+            pin1.write_digital(0)
+            pin2.write_digital(0)
+
+            
